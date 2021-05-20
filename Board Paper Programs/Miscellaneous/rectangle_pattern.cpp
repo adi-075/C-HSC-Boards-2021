d01515e8c111ec2dc7dd4ba1926e5d5d100d8728
adi-075 <adityamhambrey323@gmail.com>
2021-05-20 12:39:17 +0530

    Added Miscellaneous Programs

---
+++ Board Paper Programs/Miscellaneous/rectangle_pattern.cpp
@@ -0,0 +1,17 @@
+// Print rectangular pattern using user input 
+#include <iostream>
+using namespace std;
+int main()
+{
+  int row, col;
+  cin >> row >> col;
+  for(int i=1;i<=row;i++)
+  {
+    for(int j=1;j<=col;j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+  return 0;
+}
